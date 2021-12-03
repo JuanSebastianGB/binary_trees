5ffcf1499d76c1ebcde8557a24113eda8f3054ba
JuanSebastianGB <3393@holbertonschool.com>
2021-12-03 03:51:28 +0000

    Fix, removing leaf condition from binary height function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -24,7 +24,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_count = 0, right_count = 0;
 
-	if (tree == NULL || binary_tree_is_leaf(tree))
+	if (tree == NULL)
 		return (0);
 
 	left_count = binary_tree_height(tree->left);
@@ -42,9 +42,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
-
 	if (tree == NULL)
-		return (0);
+		return (-1);
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
